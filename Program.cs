f89ae0fa3c4819379dce41399d6367bb3b3118f3
Sergey <emilns.p@gmail.com>
2022-11-30 19:45:58 +0700

    change array

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-string[] array1 = new string[7] {"123", "23", "hello", "world", "res", "geek", "brains"};
+string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
